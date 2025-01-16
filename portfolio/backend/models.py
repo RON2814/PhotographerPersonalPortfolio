8613909132f97e154b4169154852659919ed1230
Nell Capili <bc.nelson.capili@cvsu.edu.ph>
2025-01-16 13:07:25 +0800

    uses database using python manage.py shell

--- portfolio/backend/models.py
+++ portfolio/backend/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class PortfolioProject(models.Model):
+    CATEGORY_CHOICES = [
+        ('fashion', 'Fashion'),
+        ('nature', 'Nature'),
+        ('wedding', 'Wedding'),
+        ('product', 'Product'),
+        ('studio', 'Studio'),
+        ('sports', 'Sports'),
+    ]
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to='portfolio_images/')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='fashion')
 
     def __str__(self):
         return self.title
